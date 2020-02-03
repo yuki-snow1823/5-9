e0131ebbb12e92ba4c85e465bc2d06345fe30c57
yuki-snow1823 <magic.iroha@gmail.com>
2020-02-03 19:02:07 +0900

    [fix]URLの指定と次に何するかは決まった。（プロフとそのモデル）

--- Bookers2/config/routes.rb
+++ Bookers2/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#show'
+  # git status見ながら正しいURLにくっつけた
+  resources :home, only: [:new, :create, :index, :show]
+  # 試しにやってみたが違った。ただあとで使うかもしれないから保留。
+  resources :users
 end
