6aed88407280743d51a0e9cc7a07426901b86d8a
yuki-snow1823 <magic.iroha@gmail.com>
2020-02-04 16:39:37 +0900

    [fix]みんなの投稿一覧は完成、次は自分の一覧

--- Bookers2/app/controllers/books_controller.rb
+++ Bookers2/app/controllers/books_controller.rb
@@ -1,17 +1,22 @@
 class BooksController < ApplicationController
   def new
-
   end
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    # 保存の前に、ユーザーのIDと関連してますよという記述をしてあげることで連動する！セーブの直前の行に書く
     @book.save
     redirect_to book_path(@book.id)
   end
+
   def index
+    @books = Book.all
   end
+
   def show
     @book = Book.find(params[:id]) 
+    @books = Book.all
+    # Aさんの全部の本という記述にしたい。自分の投稿一覧の章を見直す
   end
 
   private
